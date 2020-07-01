e513c38a7b44b1e45590508df808eb8c0ec9f357
Phillip Cutter <mrfleap@gmail.com>
2020-07-01 16:49:38 -0700

    Added PyPi entry point

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name='zappa-env',         # How you named your package folder (MyLib)
     packages=['zappa-env'],   # Chose the same as "name"
-    version='0.1',      # Start with a small number and increase it with every change you make
+    version='0.21',      # Start with a small number and increase it with every change you make
     # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     license='MIT',
     # Give a short description about your library
@@ -12,12 +12,17 @@ setup(
     # Provide either the link to your github or to your website
     url='https://github.com/mrfleap/zappa-env',
     # I explain this later on
-    download_url='https://github.com/mrfleap/zappa-env/archive/0.1.tar.gz',
+    download_url='https://github.com/mrfleap/zappa-env/archive/0.2.tar.gz',
     # Keywords that define your package best
     keywords=['zappa', 'virtualenv', 'env', 'virtualenvironment'],
     install_requires=[            # I get to this in a second
         'zappa',
     ],
+    entry_points={
+        'console_scripts': [
+            'zappa-env = zappa-env.zappa-env:main',
+        ],
+    },
     classifiers=[
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Development Status :: 4 - Beta',
